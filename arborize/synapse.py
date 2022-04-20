0228676a16792421b0212de647526877d2fda451
Robin De Schepper <robin.deschepper93@gmail.com>
2022-04-20 10:55:10 +0200

    don't expsyn the gap junctions

--- arborize/synapse.py
+++ arborize/synapse.py
@@ -10,8 +10,12 @@ class Synapse:
         self._point_process_name = point_process_name
         self.source = source
         with g.context(pkg=cell.__class__.glia_package):
-            self._point_process_glia_name = "ExpSyn"
-            self._point_process = p.ExpSyn(section(0.5))
+            if "gap" in point_process_name.lower():
+                self._point_process_glia_name = g.resolve(point_process_name, variant=variant)
+                self._point_process = g.insert(section, point_process_name, variant=variant)
+            else:
+                self._point_process_glia_name = "ExpSyn"
+                self._point_process = p.ExpSyn(section(0.5))
         section.__ref__(self)
         for key, value in attributes.items():
             setattr(self._point_process, key, value)
